0a6b520642a7d7c20e3efc868726759ac27f2dca
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2020-12-31 01:14:11 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@
 #include"input.h"
 #include"move.h"
 #include"wordgame.h"
+#include"moveascii.h"
 
 using namespace std;
 
 int plays=0,wins=0;
+int grid_flag[50][25];
+
 int main()
 {
-
+    int c=120;
+    srand(time(NULL));
     mciSendString("seek BG.mp3 to start", NULL, 100, NULL);
     mciSendString("play BG.mp3", NULL, 0, NULL);
     //PlaySound(TEXT("welcome.wav"), NULL, SND_SYNC);
-
-    srand(time(NULL));
     movement();
     WorL();
     mciSendString("close BG.mp3", NULL, 0, NULL);
