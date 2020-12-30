219a6aea7283fecbd8b5144c1d404f0c8648d671
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2020-12-31 01:14:13 +0600

    Update move.h

--- move.h
+++ move.h
@@ -4,9 +4,11 @@
 #include"input.h"
 #include"grid.h"
 #include"wordgame.h"
+#include"moveascii.h"
 
 using namespace std;
-extern int plays,wins; //to use in all source files
+extern int plays,wins;
+extern int grid_flag[50][25]; //to use in all source files
 
 position check(position);
 void movement()
@@ -14,19 +16,31 @@ void movement()
     grid jungle {50, 25};   //jungle.width=10;jungle.height=10;
     //system("chcp 65001  > nul");
     position pos;
-    pos.px = 0;
-    pos.py = 0;
-    pos.wx1=rand()%49+1; //range 1 to 49
-    pos.wy1=rand()%24+1;
-    pos.wx2=rand()%49+1;
-    pos.wy2=rand()%24+1;
-    pos.wx3=rand()%49+1;
-    pos.wy3=rand()%24+1;
-    pos.wx4=rand()%49+1;
-    pos.wy4=rand()%24+1;
+    pos.px = 1;
+    pos.py = 1;
+    pos.wx1=rand()%48+1; //range 1 to 48
+    pos.wy1=rand()%23+1;
+    grid_flag[pos.wx1][pos.wy1]=1;
+    pos.wx2=rand()%48+1;
+    pos.wy2=rand()%23+1;
+    grid_flag[pos.wx2][pos.wy2]=1;
+    pos.wx3=rand()%48+1;
+    pos.wy3=rand()%23+1;
+    grid_flag[pos.wx3][pos.wy3]=1;
+    pos.wx4=rand()%48+1;
+    pos.wy4=rand()%23+1;
+    grid_flag[pos.wx4][pos.wy4]=1;
+    pos.tx1=rand()%48+1;
+    pos.ty1=rand()%23+1;
+    pos.tx2=rand()%48+1;
+    pos.ty2=rand()%23+1;
+    pos.tx3=rand()%48+1;
+    pos.ty3=rand()%23+1;
+    pos.tx4=rand()%48+1;
+    pos.ty4=rand()%23+1;
 
     playerinput p;
-
+//cout<<pos.tx1<<" "<<pos.ty1<<" "<<pos.tx2<<" "<<pos.ty2<<endl;
     char input = '0';
     while (plays<4)
     {
@@ -38,26 +52,26 @@ void movement()
         if (input == 'w') //up
         {
             pos.py--;
-            if(pos.py<0)
-                pos.py=24;
+            if(pos.py<=0)
+                pos.py=23;
         }
         else if (input == 's') //down
         {
             pos.py++;
-            if(pos.py>24)
-                pos.py=0;
+            if(pos.py>=24)
+                pos.py=1;
         }
         else if (input == 'd') //right
         {
             pos.px++;
-            if(pos.px>49)
-                pos.px=0;
+            if(pos.px>=49)
+                pos.px=1;
         }
         else if (input == 'a') //left
         {
             pos.px--;
-            if(pos.px<0)
-                pos.px=49;
+            if(pos.px<=0)
+                pos.px=48;
         }
         else if(input == 'q')
         {
@@ -72,6 +86,7 @@ void movement()
         pos=check(pos);
     }
 }
+
 //checks for all of the pos values
 position check(position pos)
 {
@@ -95,6 +110,12 @@ position check(position pos)
     {
         newpos.wx4=-333;
         hangman();
+    }
+    else if(pos.px==pos.tx1 && pos.py==pos.ty1 || pos.px==pos.tx2 && pos.py==pos.ty2 || pos.px==pos.tx3 && pos.py==pos.ty3 || pos.px==pos.tx4 && pos.py==pos.ty4)
+    {
+        newpos.tx1=-333;
+        plays+=4;
+        movebear();
     }
 
     return newpos;
