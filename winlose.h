55aa69f10bbf56bf57c4eefe413b292fb71dd047
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-03 00:58:53 +0600

    Update winlose.h

--- winlose.h
+++ winlose.h
@@ -6,22 +6,22 @@
 
 extern int plays,wins,points,level;
 char ch;
-movement();
 void WorL()
 {
-    if(wins>=3)
+    if(wins>=3) //time ache, min solved
     {
         level++;
         cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
         cout<<"\n\t\t"<<"You moved to the next level"<<endl;
+        //movement();
     }
-    else if(level==3)
+    else if(level==3) //all level complete
     {
         printf("\n\n\t\tWooHoo!! You won! :)");
         cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
         PlaySound(TEXT("won.wav"), NULL, SND_SYNC);
     }
-    else if(plays>=4 && wins<4)
+    else if(plays>=4 && wins<4) //time ache, min unsolved
     {
         //printf("\n\n\t\tOh no!! You lose! :(\n");
         cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
@@ -41,9 +41,9 @@ void WorL()
             cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
             PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
         }
-        PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
+        //PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
     }
-    else if(time_left==0)
+    else if(time_left==0) //time shesh
     {
         printf( "\n\n\t\tTime's out!\n");
         cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
