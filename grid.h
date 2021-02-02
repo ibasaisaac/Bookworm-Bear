5fba15e83f4545fe6d0b156c0a9fe74c1485fed2
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-02 19:45:41 +0600

    Update grid.h

--- grid.h
+++ grid.h
@@ -1,12 +1,12 @@
 #ifndef GRID_H_INCLUDED
 #define GRID_H_INCLUDED
 using namespace std;
-extern int grid_flag[50][25];
+extern int grid_flag[50][25],level;
 struct position
 {
     int px, py;
-    int wx1,wx2,wx3,wx4;
-    int wy1,wy2,wy3,wy4;
+    int wx1,wx2,wx3,wx4,wx5,wx6,wx7,wx8,wx9,wx10;
+    int wy1,wy2,wy3,wy4,wy5,wy6,wy7,wy8,wy9,wy10;
     int tx1,tx2,tx3,tx4,ty1,ty2,ty3,ty4;
 };
 struct grid
@@ -28,7 +28,10 @@ struct grid
                 {
                     cout<<"W";
                 }
-
+                else if(level==2 && (col==pos.wx5 && row==pos.wy5 || col==pos.wx6 && row==pos.wy6))
+                {
+                    cout<<"W";
+                }
                 else if(grid_flag[row][col]!=1 && (col==pos.tx1 && row==pos.ty1 || col==pos.tx2 && row==pos.ty2 || col==pos.tx3 && row==pos.ty3 || col==pos.tx4 && row==pos.ty4))
                 {
                     cout<<"w";
