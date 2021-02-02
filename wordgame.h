e7ff426932698a8cbf16c450b8316d92db247c52
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-02 18:51:28 +0600

    Update wordgame.h

--- wordgame.h
+++ wordgame.h
@@ -9,8 +9,7 @@
 #include"timer.h"
 
 extern int plays,wins,points;
-char ch;
-//void movement();
+
 bool load(string file_name, vector<string> &list)
 {
     fstream fp(file_name, ios::in);
@@ -189,54 +188,5 @@ void hangman()
 
     system("CLS");
 }
-void WorL()
-{
-    if(wins==plays)
-    {
-        printf("\n\n\t\tWooHoo!! You won! :)");
-        cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-        PlaySound(TEXT("won.wav"), NULL, SND_SYNC);
-//        cout<<"\n\n\n"<<"Do you want to continue? (Y/N)"<<endl;
-//        cout<<"Enter choice: ";
-//        cin>>ch;
-//        if(toupper(ch)=='Y')
-//        {
-//            wins=0;
-//            plays=0;
-//        }
-//        else
-//        {
-//            printf("\n\n\t\tOh no!! You lose! :(\n");
-//            cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-//            PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
-//        }
-    }
-    else if(plays>4 && wins<2)
-    {
-        //printf("\n\n\t\tOh no!! You lose! :(\n");
-        cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-        cout<<"Restart the level? (Y/N)"<<endl;
-        cout<<"Enter choice: ";
-        cin>>ch;
-        if(toupper(ch)=='Y')
-        {
-            wins=0;
-            plays=0;
-        }
-        else
-        {
-            printf("\n\n\t\tOh no!! You lose! :(\n");
-            cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-            PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
-        }
-        PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
-    }
-    else if(time_left==0)
-    {
-        printf( "\n\n\t\tTime's out!\n");
-        cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-        printf("\n\n\t\tOh no!! You lose! :(\n");
-        PlaySound(TEXT("lose.wav"), NULL, SND_SYNC);
-    }
-}
+
 #endif // WORDGAME_H_INCLUDED
