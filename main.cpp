c6c05d19727d91a209d623859de3986f4d553254
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-05 04:29:16 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,35 @@
 #include<conio.h>
 #include"grid.h"
 #include"color.h"
-#include"input.h"
 #include"move.h"
 #include"wordgame.h"
 #include"moveascii.h"
 #include"timer.h"
-#include"winlose.h"
 using namespace std;
 
 int plays=0,wins=0,points=6,level=1;
-int grid_flag[50][25];
+
 
 int main()
 {
     mciSendString("seek BG.mp3 to start", NULL, 100, NULL);
     mciSendString("play BG.mp3", NULL, 0, NULL);
-    srand(time(NULL));
+    //loading words
+    if (!load("words.txt", wordlist))
+    {
+        printf("words.txt is missing\n");
+        exit(1);
+    }
+
+    //loading hints
+    if (!load("hints.txt", hintlist))
+    {
+        printf("hints.txt missing\n");
+        exit(1);
+    }
+    //srand(time(NULL));
     movement();
     //WorL();
-    mciSendString("close BG.mp3", NULL, 0, NULL);
+    //mciSendString("close BG.mp3", NULL, 0, NULL);
     return 0;
 }
