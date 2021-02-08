c895f132e04ebdd299762ca6e3262365b3e437c9
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-08 20:26:43 +0600

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,11 +1,11 @@
 #ifndef TIMER_H_INCLUDED
 #define TIMER_H_INCLUDED
+
 #include<cstdlib>
 #include<ctime>
+
 clock_t x_startTime, x_countTime;
 unsigned int x_milliseconds=0, count_down_time_in_secs=0, time_left=0, pause_time=0;
-
-void gotoxy(int x, int y);
 
 void  delta_time_update_timer()
 {
@@ -13,25 +13,8 @@ void  delta_time_update_timer()
     x_milliseconds=x_countTime-x_startTime;
     time_left=count_down_time_in_secs-(x_milliseconds/CLOCKS_PER_SEC);
 
-    gotoxy(1, 1);
-    printf( "\nTime remaining: %d:%d", time_left/60, time_left-((time_left/60)*60));
-    gotoxy(1,5);
+    gotoxy(1, 2);
+    printf("\nTime remaining: %d:%d ", time_left/60, time_left-((time_left/60)*60));
 }
-void gotoxy(int x, int y)
-{
-    static HANDLE hStdout = NULL;
-    COORD coord;
-
-    coord.X = x;
-    coord.Y = y;
-
-    if(!hStdout)
-    {
-        hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    }
-
-    SetConsoleCursorPosition(hStdout,coord);
-}
-
 
 #endif // TIMER_H_INCLUDED
