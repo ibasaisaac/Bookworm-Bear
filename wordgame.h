b7948ad3e6ca562f544deaadb66097d6e1e41849
Isaba Ishrak <62184424+ibasaisaac@users.noreply.github.com>
2021-02-08 20:26:56 +0600

    Update wordgame.h

--- wordgame.h
+++ wordgame.h
@@ -1,41 +1,14 @@
 #ifndef WORDGAME_H_INCLUDED
 #define WORDGAME_H_INCLUDED
 
-#include<cstdlib>
-#include<windows.h>
-#include<mmsystem.h>
-#include<conio.h>
-#include"color.h"
-#include"move.h"
 #include"timer.h"
 
-extern int plays,wins,points;
+extern int wins;
+vector<pair<string, string>> wordlist;
+int serial[20]= {0}, s=0;
 
-vector<string> wordlist;
-vector<string> hintlist;
 
-
-//to load words and hints from file to vector
-bool load(string file_name, vector<string> &list) //list passing the address of wordlist and hintslist
-{
-    ifstream fp(file_name, ios::in);
-    string buffer;
-
-    if(fp.is_open())
-    {
-        list.clear();
-        while(!fp.eof())
-        {
-            getline(fp, buffer);
-            list.push_back(buffer);
-        }
-        fp.close();
-        return 1;
-    }
-    else
-        return 0;
-}
-
+//hangman gameplay
 int hangman(void)
 {
     x_countTime=clock();
@@ -45,52 +18,16 @@ int hangman(void)
     //loading fresh letters for reference
     vector<char> alphabet;
     for (int i = 65; i <=90; i++)
-        alphabet.push_back(i); //all alphabets and cross out the ones used
+        alphabet.push_back(i);
 
-    //choosing random word
-    srand(time(0));
-    string word;
-    word = wordlist.at(rand() % wordlist.size());
+    //choosing random word and hint
+    string word, hint;
+    word = wordlist[serial[s]].first;
+    hint=wordlist[serial[s]].second;
+    s++;
 
-//    //loading words
-//    vector<string> wordlist;
-//    if (!load("words.txt", wordlist))
-//    {
-//        printf("words.txt is missing\n");
-//        exit(1);
-//    }
-//
-//    //loading hints
-//    vector<string> hintlist;
-//    if (!load("hints.txt", hintlist))
-//    {
-//        printf("hints.txt missing\n");
-//        exit(1);
-//    }
-
-
-    int serial;
-
-    //erasing word from vector to prevent repetition and getting the correspondent hint
-    vector<string>::iterator itr = std::find(wordlist.begin(), wordlist.end(), word);
-    if (itr != wordlist.end())
-    {
-        serial = (itr - wordlist.begin())+1;
-        wordlist.erase(itr);
-    }
-
-    string hint = hintlist[serial-1];
-
-    //erasing the serial hint from hinlist
-    itr = find(hintlist.begin(), hintlist.end(), hint);
-    if (itr != hintlist.end())
-    {
-        hintlist.erase(itr);
-    }
-
-    int total_letters, filled_blanks = 0, lives = 6, flag = 0, h = 1, points=6;
-    char current_blanks[25], hint_ch='n', letter1, letter2;
-    total_letters = word.size();
+    int total_letters= word.size(), filled_blanks = 0, lives = 6, points = 6, flag = 0, h = 1;
+    char current_blanks[10], hint_ch='n', letter1, letter2;
     for (int i = 0; i < total_letters; i++)
         current_blanks[i] = '_';
 
@@ -100,53 +37,62 @@ int hangman(void)
     {
         flag = 0;
         letter1='0';
-        SetColor(PINK);
-        printf("\n\n\t\t\t WORD PLAY \t\t\tlives: %d/6", lives);
+
+        SetColor(lightred);
+        printf("\n\n\t\t\t\t Word Game \t\t\tlives: %d/6", lives);
+
 
         //hint
-        SetColor(WHITE);
+        SetColor(lightaqua);
+        cout<<"\n\n";
         if(lives<3 && h==1)
         {
-            printf("\n\n\t Need a hint?(Y/N) ");
+            printf("\tNeed a hint?(Y/N) ");
             scanf("%c", &hint_ch);
             getchar();
         }
 
         if(hint_ch=='Y' || hint_ch=='y')
         {
-            cout<<"\n\n\t"<<hint<<"\n";
+            SetColor(white);
+            cout<<"\n\n\t"<<hint;
             h=0;
         }
 
+
         //printing blanks
-        printf("\n\n\t ");
+        printf("\n\n\n\n\t");
         for (int i = 0; i < total_letters; i++)
         {
-            SetColor(DARKBLUE);
+            SetColor(lightyellow);
             printf("%c ", current_blanks[i]);
         }
 
+
         //printing reference alphabet
-        SetColor(TEAL);
+        SetColor(darkaqua);
         printf("\n\n\n\t ");
-        for (auto j = alphabet.begin(); j != alphabet.end(); j++)
-            cout << ' ' << *j;
+        for (auto j = alphabet.begin(); j != alphabet.end(); j++)   cout << " " << *j;
 
-        while(isalpha(letter1)==0) //until letters entered
+
+
+        //taking input and replacing used letter with symbol for reference
+        SetColor(lightaqua);
+        while(isalpha(letter1)==0)
         {
-            printf("\n\n\t Guess a letter: ");
-
+            printf("\n\n\tGuess a letter: ");
             scanf("%c", &letter1);
             getchar();
         }
         letter2=toupper(letter1);
-        std::replace (alphabet.begin(), alphabet.end(), letter2, (char)177); //crossing out the guessed letters
+        replace (alphabet.begin(), alphabet.end(), letter2, (char)177);
+        SetColor(white);
 
 
-        //checking input letter with word and updating blanks
+        //checking input with word and updating blanks
         for (int i = 0; i < total_letters; i++)
         {
-            if (tolower(letter1) == word[i])
+            if (tolower(letter1)==word[i])
             {
                 flag = 1;
                 current_blanks[i] = word[i];
@@ -155,22 +101,21 @@ int hangman(void)
         }
 
 
-        //correctly guessed
+
+        //guessed a letter correctly
         if (flag == 1)
         {
-            SetColor(YELLOW);
-            printf("\n\n\t\tYou got it!");
-            PlaySound(TEXT("lilwin.wav"), NULL, SND_SYNC);
+            printf("\n\n\t\t\t\tYou got it!");
+            PlaySound(TEXT("lettercorrect.wav"), NULL, SND_FILENAME | SND_SYNC);
         }
-        //could not guess correctly
+        //guessed a letter incorrectly
         else
         {
             lives--;
             points--;
-            SetColor(DARKPINK);
-            printf("\n\n\t\t%c is not here!", letter1);
-            printf("\n\n\t\t%d guesses remaining", lives);
-            PlaySound(TEXT("lillose.wav"), NULL, SND_SYNC);
+            printf("\n\n\t\t\t\t%c is not here!", letter1);
+            printf("\n\n\t\t\t      %d guesses remaining", lives);
+            PlaySound(TEXT("letterwrong.wav"), NULL, SND_FILENAME | SND_SYNC);
         }
 
         system("CLS");
@@ -178,31 +123,76 @@ int hangman(void)
             break;
     }
 
-    //guessed the word
+
+
+    //guessed whole word
     if (filled_blanks == total_letters)
     {
-        SetColor(GREEN);
-        cout<<"\n\n\t\t"<<"Good job"<<endl<<endl;
-        cout << "\n\n\t\t" << "The word is " << word << endl;
-        //cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-        PlaySound(TEXT("bigwin.wav"), NULL, SND_SYNC);
-        wins++; //new
+        cout << "\n\n\n\t\t\t" << "   The word is " << word << "\n\n";
+        PlaySound(TEXT("wordcorrect.wav"), NULL, SND_FILENAME | SND_SYNC);
+        wins++;
     }
-    //could not guess the word
+    //couldn't guess word
     else
     {
-        SetColor(BLUE);
-        cout << "\n\n\t\t" << "The word was " << word << "\n\n";
-        //cout<<"\n\t\t"<<"You have "<<points<<" points"<<"\n\n";
-        PlaySound(TEXT("biglose.wav"), NULL, SND_SYNC);
+        cout << "\n\n\n\t\t\t" << "   The word was " << word << "\n\n";
+        PlaySound(TEXT("wordwrong.wav"), NULL, SND_FILENAME | SND_SYNC);
     }
-    SetColor(WHITE);
+
+
+    SetColor(white);
+
     x_startTime=clock();
-    count_down_time_in_secs-=pause_time;
+    count_down_time_in_secs-= pause_time;
 
     system("CLS");
-
     return points;
+}
+
+//to load words and hints from file to vector
+bool load(string word_file, string hint_file, vector<pair<string,string>> &list)
+{
+    list.clear();
+    string buffer1, buffer2;
+    int i=0;
+    ifstream fp1(word_file, ios::in);
+    ifstream fp2(hint_file, ios::in);
+
+    if(fp1.is_open() && fp2.is_open())
+    {
+        while(!fp1.eof())
+        {
+            getline(fp1, buffer1);
+            list.push_back(make_pair(buffer1, " "));
+        }
+        fp1.close();
+
+        while(!fp2.eof())
+        {
+            getline(fp2, buffer2);
+            list[i].second=buffer2;
+            i++;
+        }
+        fp2.close();
+
+        srand(time(0));
+        int generated[list.size()]= {0};
+        int count = 0, num;
+
+        while (count<list.size())
+        {
+            num = rand()%list.size();
+
+            if (generated[num] == 0)
+            {
+                generated[num] = 1;
+                serial[count++] = num;
+            }
+        }
+        return 1;
+    }
+    else
+        return 0;
 }
 
 #endif // WORDGAME_H_INCLUDED
